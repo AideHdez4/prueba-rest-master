41000ede7eff73bd8524779f012e4b177741dbeb
aide_registro <aideyerania@gmail.com>
2022-02-27 23:29:31 -0600

    Se agrega el proyecto

---
+++ src/main/java/uatx/mx/demorest/model/Persona.java
@@ -0,0 +1,12 @@
+package uatx.mx.demorest.model;
+
+/**
+ * @author edsson
+ * @date 19/02/22
+ */
+public class Persona {
+    public Integer id;
+    public String nombre;
+    public Integer edad;
+    public String sexo;
+}
